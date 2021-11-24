2766c56099418956621c6e2d40a049237a305a88
Matthew Vaden <matthewvaden@gmail.com>
2021-11-24 00:17:15 -0500

    feature/main: Minor refactor.

--- server/index.js
+++ server/index.js
@@ -12,41 +12,33 @@ const BearerToken = process.env.BEARER_TOKEN;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {    
-    const getTweets = async(id) => {
-        const endpointUrl = "https://api.twitter.com/1.1/search/tweets.json";
-        const params = {
-            "q": "weather",
-            "result_type": "recent",
-            count: 5
+const getTweets = async(req, res) => {
+    const endpointUrl = "https://api.twitter.com/1.1/search/tweets.json";
+    const params = {
+        "q": "weather",
+        "result_type": "recent",
+        count: 5
+    }
+    const headers = {
+        headers: {
+            "User-Agent": "v2RecentSearchJS",
+            "authorization": `Bearer ${BearerToken}`,
         }
-        const headers = {
-            headers: {
-                "User-Agent": "v2RecentSearchJS",
-                "authorization": `Bearer ${BearerToken}`,
-            }
-        }
+    }
+    const response = await needle ("get", endpointUrl, params, headers);
 
-        const response = await needle ("get", endpointUrl, params, headers);
-
-        if (res.statusCode !== 200) {
-            if (res.statusCode === 403) {
-                res.status(403).send(response.body);
-                console.error("Return 403.");
-            }
-            else {
-                console.log("Error was not 403.");
-            }
-        }
-
-        if (response.body) {
-            return response.body
+    if (res.statusCode !== 200) {
+        if (res.statusCode === 403) {
+            res.status(403).send(response.body);
+            console.error("Return 403.");
         }
         else {
-            console.log("Request was unsuccessful.")
+            console.log("Error was not 403.");
         }
-        // console.log(response.body.statuses);
+    }
 
+    if (response.body) {
+        console.log("chained statuses", response.body.statuses);
         const result = Object.values(response.body.statuses).map(item => ({
             "text": item.text,
             "screen_name": item.user.screen_name,
@@ -54,11 +46,16 @@ app.get("/", (req, res) => {
             "hashtags": item.user.entities.hashtags,
             // "url": item.entities.urls
         }));
-
-        console.log(result)
-
+    
+        console.log("Final Result: ", result);
         res.status(200).send({"sending this body": result})  
     }
-});
+    else {
+        console.log("Request was unsuccessful.")
+    }
+}
+
+
+app.get("/", getTweets);
 
 app.listen(port, () => console.log(`Express is listening on port ${port}`));
